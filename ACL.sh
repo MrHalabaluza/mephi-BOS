f411ff38e62d189ba9b33f36a7d8e70613b279e3
Galkina E <savaki@lenta.ru>
2016-11-18 01:53:05 +0300

    не совсем корректно работает, я ещё подредактирую

---
+++ ACL.sh
@@ -0,0 +1,123 @@
+#!/bin/bash
+#
+# Изменение ACL прав файла
+#на вход нужно подать имя файла
+filename="$1" #может быть неправильно
+
+defineRights(){
+					echo "Установить права на чтение?(y/n)"
+					read ans
+					case "$ans1" in 
+						y|Y) r_right = 'r'
+							;;
+						n|N) r_right = '-'
+							;;
+						*) 
+						echo "Неверный ввод"
+						continue
+						;;
+						esac
+
+			echo "Установить права на запись?(y/n)"
+					read ans
+					case "$ans" in 
+						y|Y) w_right = 'w'
+							;;
+						n|N) w_right = '-'
+							;;
+					*) 
+					echo "Неверный ввод"
+					continue
+					;;
+			esac
+
+			echo "Установить права на запуск?(y/n)"
+					read ans
+					case "$ans" in 
+						y|Y) x_right = 'x'
+							;;
+						n|N) x_right = '-'
+							;;
+						*)
+							echo "Неверный ввод"
+							continue
+							;;
+					esac
+					rights ="$r_right$w_right$x_right"
+}
+options=("Добавить запись" "Удалить запись" "Изменить запись" "Назад")
+select opt in "${options[@]}"
+do
+	case $opt in
+		"1")
+			echo " Добавление записи"
+			echo "Пользователь или группа? (u/g)) "
+			read ans
+			case "$ans" in
+				u|U) echo "Введите имя пользователя"
+					read username
+					defineRights
+					setfacl -m u:$username:$rights $filename
+					;;
+				g|G) echo "Введите имя группы"
+					read groupname
+					defineRights
+					setfacl -m g:$groupname:$right $filename
+					;;
+				*) 
+					echo "Неверный ввод"
+					continue
+					;;
+			esac
+			;;
+		"2")
+			echo "Удаление записи"
+			echo "Пользователь или группа? (u/g)) "
+			read ans
+			case "$ans" in
+				u|U) echo "Введите имя пользователя"
+					read username
+					setfacl -x u:$username $filename
+					;;
+				g|G) echo "Введите имя группы"
+					read groupname
+					setfacl -x g:$groupname $filename
+					;;
+				*)	 echo "Неверный ввод"
+					continue
+					;;
+			esac
+			;;
+		"3")
+			echo "Изменение записи"
+				echo "Пользователь или группа? (u/g)) "
+			read ans
+			case "$ans" in
+				u|U) echo "Введите имя пользователя"
+					read username
+					getfacl $filename
+					defineRights
+					setfacl -m u:$username:$rights $filename
+					;;
+				g|G) echo "Введите имя группы"
+					read groupname
+					getfacl $filename
+					defineRights
+					setfacl -m g:$groupname:$rights $filename
+					;;
+				*) echo "Неверный ввод"
+					continue
+					;;
+			esac
+			;;
+		"4") 
+			break
+			;;
+
+		*) echo "Неверный ввод"
+			continue
+			;;
+	esac
+	echo "Done!"
+done
+
