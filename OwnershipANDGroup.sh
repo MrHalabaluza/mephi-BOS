db0b5d411bc49602e332a687cace82f1b2ec039d
Alina Garbuz <lina454@mail.ru>
2016-11-29 04:12:08 +0300

    Меню обновлено

--- OwnershipANDGroup.sh
+++ OwnershipANDGroup.sh
@@ -3,81 +3,125 @@
 echo "Изменение владельца или группы файла"
 PS3="Выберите следующее действие:"
 options=("Изменение владельца" "Изменение группы" "Выход")
-choose=("Выполнить рекурсивно для директории" "Выполнить только для папки")
+choose=("Выполнить рекурсивно для директории" "Выполнить только для папки" "Выход")
 select opt in "${options[@]}"
 do
-	case $opt in
-		"Изменение владельца")
-			echo "Введите имя нового владельца:"			
-			read USERNAMEN
-			grep "$USERNAMEN:" /etc/passwd >/dev/null
-			if [ $? -ne 0 ]; then
-				echo "Пользователь не существует!" >&2
-			else
-				 if [ -f $1 ]; then
-					 chown $USERNAMEN: $1
-					 echo "Влвделец файла изменен успешно!"
-				 elif [ -d $1 ]; then
-					 echo "Директория! Вберите следующее действие:"
-					 select ch in "${choose[@]}"
-					 do
-						 case $ch in
-							 "Выполнить рекурсивно для директории")
-								 chown -R $USERNAMEN: $1
-								 echo "Владелец изменен у каждого файла директории!"
-								 break
-								 ;;
-							 "Выполнить только для папки")
-								 chown $USERNAMEN: $1
-								  echo "Владелец папки изменен успешно!"
-								  break
-								  ;;
-							 *) echo "Некорректный ввод!" >&2
-								 ;;
-						 esac 
-					 done
-				 else
-					 echo "Некорректный ввод!" >&2
-				 fi
-			 fi
-			 ;;
-		"Изменение группы")
-			echo "Введите название группы:"
-			read NAMEGROUP
-			grep -q -E "^$NAMEGROUP:" /etc/group >/dev/null
-			if [ $? -ne 0 ];
-			then
-				echo "Группа не существует!" >&2
-			else
-				if [ -f $1 ]; then
-					chown :$NAMEGROUP $1
-					echo "Группа файла изменена успешно!"
-				elif [ -d $1 ]; then
-					echo "Директория! Выполните следующее действие:"
-					select ch in "${choose[@]}";
-					do
-						case $ch in
-							"Выполнить рекурсивно для директории")
-								chown -R :$NAMEGROUP $1
-								echo "Группа директории изменена для каждого файла директории!!"
-								break
-								;;
-							"Выполнить только для папки")
-								chown :$NAMEGROUP $1
-								echo "Группа папки изменена успешно!"
-								break
-								;;
-							*) echo "Некорректный ввод!" >&2
-								;;
-						esac
-					done
+	case $REPLY in
+		1)
+			while [ 1 ];
+			do				
+				echo "Введите имя нового владельца:"				
+				read USERNAMEN
+				grep -i "^$USERNAMEN:" /etc/passwd >/dev/null
+				if [ $? -ne 0 ]; 
+				then
+					echo "Пользователь не существует" >&2
+					echo "Повторить? (y/n)"
+					read ANSWER
+					if [ "$ANSWER" == "n" ];
+					then 
+						break
+					fi
 				else
-					echo "Некорректный ввод!" >&2
+					if [ -f $1 ]; then
+						 chown $USERNAMEN: $1
+						echo "Влвделец файла изменен успешно!"
+						break
+					elif [ -d $1 ]; then
+						 echo "Директория! Вберите следующее действие:"
+						select ch in "${choose[@]}"
+						do
+							case $REPLY in
+								1)  
+									chown -R $USERNAMEN: $1
+									echo "Владелец изменен у каждого файла директории!"
+									break
+									;;
+								2)
+									chown $USERNAMEN: $1
+									echo "Владелец папки изменен успешно!"
+									break
+									;;
+								3)
+									break
+									;;
+								"help") echo "Выберите следующее действие:
+1) Выполнить рекурсивно для всей директории
+2) Выполнить только для папки
+3) Вернуться"
+									;;
+								*) echo "Некорректный ввод!" >&2
+									 ;;
+							esac
+						done
+						break
+					else
+						 echo "Некорректный ввод!" >&2
+					fi
 				fi
-			fi
+			done
 			;;
-		"Выход")
+			2)
+				while [ 1 ];
+				do
+					echo "Введите название группы:"
+				read NAMEGROUP
+				grep -i "^$NAMEGROUP:" /etc/group >/dev/null
+				if [ $? -ne 0 ];				
+				then
+					echo "Группа не существует!" >&2
+					echo "Повторить? (y/n)"
+					read ANSWER
+					if [ "$ANSWER" == "n" ];
+					then
+						break
+					fi
+				else
+					if [ -f $1 ]; then
+						chown :$NAMEGROUP $1
+						echo "Группа файла изменена успешно!"
+						break
+					elif [ -d $1 ]; then
+						echo "Директория! Выполните следующее действие:"
+						select ch in "${choose[@]}";
+						do
+							case $REPLY in
+								1)
+									chown -R :$NAMEGROUP $1
+									echo "Группа директории изменена для каждого файла директории!!"
+									break
+									;;
+								2)
+									chown :$NAMEGROUP $1
+									echo "Группа папки изменена успешно!"
+									break
+									;;
+								3)
+									break
+									;;
+								"help") echo "Выберите следующее действие:
+1) Выполнить рекурсивно для директории
+2) Выполнить только для папки
+3) Вернуться"
+									;;
+								*) echo "Некорректный ввод!" >&2
+									;;
+							esac
+						done
+						break
+					else
+						echo "Некорректный ввод!" >&2
+					fi
+				fi
+			done
+			;;
+		3)
 			break
+			;;
+		"help") echo "Выберите следующее действие
+1) Изменение владельца
+2) Изменение группы
+3) Вернуться"
 			;;
 		*) echo "Некорректный ввод!" >&2
 			;;
